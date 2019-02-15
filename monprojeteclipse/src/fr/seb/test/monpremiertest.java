329bf55f0c187ac049b9088a124345af39e6d329
c18004286 <c18004286@a-f004248.salsa.univ-amu.fr>
2019-02-15 14:13:24 +0100

    modif

--- monprojeteclipse/src/fr/seb/test/monpremiertest.java
+++ monprojeteclipse/src/fr/seb/test/monpremiertest.java
@@ -1,5 +1,18 @@
 package fr.seb.test;
 
 public class monpremiertest {
+	
+	boolean test;
+	
+	public boolean thetest() {
+		if(test==true) {
+			System.out.println("hello !");
+			return true;
+		}
+		else {
+			System.out.println("sale nul ! ");
+			return false;
+		}
+	}
 
 }
